14d8b2d3c0b2298565bc9edf14d4fa66e76fbf6f
Hengpanharanith <122364695+Hengpanharanith@users.noreply.github.com>
2025-04-02 23:48:54 +0700

    W8---PRACTICE---Finish-the-CRUD

---
+++ lib/data/dto/music_dto.dart
@@ -0,0 +1,18 @@
+// MODEL & DTO
+
+import 'package:week8_crud/model/song.dart';
+
+class SongDto {
+  static Song fromJson(String id, Map<String, dynamic> json) {
+    return Song(
+      id: id,
+      title: json['title'] ?? '',
+      artist: json['artist'] ?? '',
+      album: json['album'] ?? '',
+    );
+  }
+
+  static Map<String, dynamic> toJson(Song song) {
+    return {'title': song.title, 'artis': song.artist, 'album': song.album};
+  }
+}
---
+++ lib/data/repository/music_repository.dart
@@ -0,0 +1,8 @@
+import 'package:week8_crud/model/song.dart';
+
+abstract class SongRepository {
+  Future<Song> addSong({required String title, required String artist, required String album});
+  Future<List<Song>> getSongs();
+  Future<void> deleteSong(String id);
+  Future<void> updateSong(Song song);
+}
